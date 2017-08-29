94675896c83e075a9fd2dccb54b50f0909fa59ad
shane98c <shane98c@gmail.com>
2017-08-29 17:26:54 -0500

    fix Thing

--- Regions2Wiki.js
+++ Regions2Wiki.js
@@ -11,7 +11,7 @@ String.prototype.toProperCase = function () {
   return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 };
 
-for (i = 0; i < 300; i++) {
+for (i = 600; i < 900; i++) {
   var name = regions.features[i].properties.name.toProperCase();
   promiseArray.push(requestWiki(name, i));
 }
@@ -24,7 +24,7 @@ Promise.all(promiseArray)
 function addNames(response) {
   for (i=0; i < response.length; i++) {
     var data = response[i];
-    if (data.query.pages == -1) {
+    if (data.query.pages['-1']) {
       console.log('fail');
       //do something to original (or copy of!) regions object to indicate that name isn't working.
       //Also add the name that was modified above to the object as "wikiName" or something
