270ea995586a4cc8f0dd868365cd2b08b13a13fd
shane98c <shane98c@gmail.com>
2018-02-04 17:30:54 -0600

    new regions.  try async/await

--- Regions2Wiki.js
+++ Regions2Wiki.js
@@ -1,43 +1,46 @@
-var regions = require('./regions.json');
-var request = require('request');
-var rp = require('request-promise');
-var fs = require('fs');
+let regions = require('./regions.json');
+let rp = require('request-promise');
 
-var regionsWiki = regions;
-var noWiki = [];
-var promiseArray = [];
+let goodWiki = [];
+let badWiki = [];
 
 String.prototype.toProperCase = function () {
   return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 };
 
-for (i = 600; i < 900; i++) {
-  var name = regions.features[i].properties.name.toProperCase();
-  promiseArray.push(requestWiki(name, i));
+for (let region of regions.features) {
+  let name = region.properties.name.toProperCase();
+  loadParse(name, region);
 }
 
-Promise.all(promiseArray)
-  .then(response => {
-    addNames(response);
-  })
-
-function addNames(response) {
-  for (i=0; i < response.length; i++) {
-    var data = response[i];
-    if (data.query.pages['-1']) {
-      console.log('fail');
-      //do something to original (or copy of!) regions object to indicate that name isn't working.
-      //Also add the name that was modified above to the object as "wikiName" or something
-    } else {
-      console.log('success')
-      //do something to original (or copy of!) regions object to indicate that name IS working.
-      //Also add the name that was modified above to the object as "wikiName"
-    }
+async function loadParse (name, region) {
+  try {
+    let response = await requestWiki(name);
+    addNames(response, name, region);
+  } catch (error){
+    console.error(error);
   }
 }
 
-function requestWiki(name, i) {
-  var url = ['https://en.wikipedia.org/w/api.php?action=query&prop=extracts&redirects=1&format=json&titles=', name].join('');
+function addNames(resp, name, region) {
+  if (resp.query.pages['-1']) {
+    // console.error('fail');
+    badWiki.push(region);
+    //do something to original (or copy of!) regions object to indicate that name isn't working.
+    //Also add the name that was modified above to the object as "wikiName" or something
+  } else {
+    // console.log('success');
+    for (let page in resp.query.pages) {
+      // console.log(resp.query.pages[page].title, name);
+      goodWiki.push(region);
+    }
+    //do something to original (or copy of!) regions object to indicate that name IS working.
+    //Also add the name that was modified above to the object as "wikiName"
+  }
+}
+
+function requestWiki(name) {
+  let url = ['https://en.wikipedia.org/w/api.php?action=query&prop=extracts&redirects=1&format=json&titles=', name].join('');
   return rp({
     uri: url,
     json: true
