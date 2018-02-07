e23361f91e50491a71ef77c48f281ecca2464149
shane98c <shane98c@gmail.com>
2018-02-06 22:57:05 -0600

    pbdb queries working

--- checkWiki.js
+++ checkWiki.js
@@ -1,39 +1,54 @@
-const regions = require("./regions.json");
 const rp = require("request-promise");
 const fs = require("fs");
 
 const goodWiki = [];
 const badWiki = [];
 
-String.prototype.toProperCase = function() {
-  return this.replace(/\w\S*/g, function(txt) {
-    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-  });
-};
+getPbdb();
 
-regions.features.map((region, index) => {
-  const { name } = region.properties;
-  setTimeout(() => {
-    checkWiki(name.toProperCase(), region);
-  }, 1 * index);
-});
-
-async function checkWiki(name, region) {
+async function getPbdb() {
+  console.log("requesting data...");
+  const url =
+    "https://paleobiodb.org/data1.2/occs/list.json?base_name=dinosauria^aves";
   try {
-    let response = await requestWiki(name);
-    addNames(response, name, region);
+    let pbdb = await rp({ uri: url, json: true });
+    loopPbdb(pbdb);
   } catch (error) {
     console.error(error);
   }
 }
 
-function addNames(resp, name, region) {
+function loopPbdb(pbdb) {
+  console.log(pbdb.records.length, "records in this query");
+  console.log("filtering duplicates...");
+  pbdbDeDoop = pbdb.records.filter(
+    (data, index, self) => self.findIndex(t => t.tna === data.tna) === index
+  );
+  console.log(pbdbDeDoop.length, "unique taxa in this query");
+  console.log("checking for Wiki articles...");
+  pbdbDeDoop.map((record, index) => {
+    const { tna } = record;
+    setTimeout(() => {
+      checkWiki(tna);
+    }, 10 * index);
+  });
+}
+
+async function checkWiki(name) {
+  try {
+    let response = await requestWiki(name);
+    addNames(response, name);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function addNames(resp, name) {
   if (resp.query.pages["-1"]) {
-    badWiki.push(region);
+    badWiki.push(name);
   } else {
     for (const page in resp.query.pages) {
-      region["wikiName"] = resp.query.pages[page].title;
-      goodWiki.push(region);
+      goodWiki.push(name);
     }
   }
 }
@@ -57,6 +72,8 @@ function writeFiles(name, data) {
 process.on("exit", finish.bind());
 
 function finish() {
+  console.log(badWiki.length, "missing Wiki articles");
+  console.log(goodWiki.length, "successful Wiki articles");
   writeFiles("badWiki.json", JSON.stringify(badWiki));
   writeFiles("goodWiki.json", JSON.stringify(goodWiki));
 }
